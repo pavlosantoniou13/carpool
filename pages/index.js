4001788f61c2b566c704c6e183aceff3205498b1
Pavlos Antoniou <dinant11133@gmail.com>
2023-04-12 01:29:58 +0300

    home pagfe ready

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import tw from "styled-components"
 import Map from "./components/map"
 import UberLogoImg from './assets/Uber_logo_2018.png'
 import UserImg from './assets/IMG-8016.jpg'
+import carImg from './assets/UberX.webp'
+import list from './assets/list.png'
 
 
 
@@ -12,20 +14,39 @@ export default function Home() {
   return (
     <Wrapper className="flex flex-col  h-screen">
       <Map></Map>
-      <ActionItems className="flex-1">
+      <ActionItems className="flex-1 p-4">
         {/* header */}
-        <Header className="flex justify-between items-center">
-          <UberLogo className="h-28 " src={UberLogoImg.src} />
+        <Header className="flex justify-between items-center m-5 w-[100%]">
+          <UberLogo className="h-12 " src={UberLogoImg.src} />
           <Profile className="flex items-center">
             <Name className="mr-4 w-20 text-sm">Pavlos Antoniou</Name>
-              <UserImage className="h-16 w-16 rounded-full border-gray-200 p-px"
+              <UserImage className="h-16 w-16 rounded-full border-gray-200 p-px mr-2"
               src={UserImg.src}/>
           </Profile>
 
         </Header>
-        {/* ActionButtons */}
+        <ActionButtons className="flex ">
 
-        {/* inputButton */}
+          <ActionButton className="text-xl font-bold transition cursor-pointer transform hover:scale-105 rounded-lg justify-center flex-1 m-1 bg-gray-200 h-32  flex items-center flex-col">
+            <ActionButtonImage className="h-3/5" src={carImg.src}/>
+           Ride
+          </ActionButton>
+
+          <ActionButton className="text-xl font-bold transition cursor-pointer transform hover:scale-105 rounded-lg justify-center flex-1 m-1 bg-gray-200 h-32 flex items-center flex-col">
+          <ActionButtonImage className="h-3/5" src={carImg.src}/>
+            All  Rides
+          </ActionButton>
+
+          <ActionButton className="text-xl font-bold transition cursor-pointer transform hover:scale-105 rounded-lg justify-center flex-1 m-1 bg-gray-200 h-32 flex items-center flex-col">
+          <ActionButtonImage className="h-3/5" src={carImg.src}/>
+            Post a ride
+          </ActionButton>
+
+        </ActionButtons>
+           
+        <InputButton className="mt-8 flex items-center font-bold p-4 text-2xl h-20 bg-gray-200">
+          Where to?
+        </InputButton>
 
       </ActionItems>
     </Wrapper>
@@ -40,3 +61,7 @@ const UberLogo = tw.img``
 const Profile = tw.div``
 const Name = tw.div``
 const UserImage = tw.img``
+const ActionButtons = tw.div``
+const ActionButton = tw.div``
+const ActionButtonImage = tw.img``
+const InputButton = tw.div``
