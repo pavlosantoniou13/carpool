efa82df8f20d46d364c97a321b6963f8d828af1f
Pavlos Antoniou <dinant11133@gmail.com>
2023-05-20 20:03:44 +0300

    Update addRideComponent.js

--- pages/components/addRideComponent.js
+++ pages/components/addRideComponent.js
@@ -30,6 +30,7 @@ function placeSuggest() {
     const [milage, setMilage] = useState()
     const [distance, setDistance] = useState()
     const [gasPriceData, setGasPriceData] = useState()
+    const [passengers, setPassengers] = useState()
 
     
     const [autocompleteCities, setAutocompleteCities] = useState([]);
@@ -149,6 +150,7 @@ function placeSuggest() {
           destination: destination.toString().toLowerCase(),
           carBrand: carBrand.toString().toLowerCase(),
           fuelType: fuelType.toString().toLowerCase(),
+          passengers: passengers.toString().toLowerCase(),
           price: price.toString() + "€",
           userId: id,
           user: userName,
@@ -179,6 +181,7 @@ function placeSuggest() {
         setPrice("");
         setDistance("");
         setMilage("");
+        setPassengers("")
         
       } else {
         toast.error("Please make sure all of the inputs are full", {
@@ -288,6 +291,24 @@ function placeSuggest() {
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Car brand
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              onChange={(e) => setPassengers(e.target.value)}
+              value={passengers}
+              type="text"
+              name="passengers"
+              id="passengers"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="fuelType"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+             Passengers
             </label>
           </div>
           <div className="relative z-0 w-full mb-6 group">
